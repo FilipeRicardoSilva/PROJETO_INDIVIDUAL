4e845cdb3520bd89fdfc627309fb9e2d796d7fd9
FilipeRicardoSilva <filipe.silva@sptech.school>
2022-11-23 11:18:04 -0300

    Configurando API

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "ARTE_GREGA",
-    user: "root",
-    password: "",
+    database: "arte_grega",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
